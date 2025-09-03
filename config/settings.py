0e9461c64feb2b20812b4d40121e0465bad884bd
maksro <romancov_dzr@mail.ru>
2025-09-03 23:37:01 +0300

    correct math formula

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", False) == "True"
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from .forms import HeightForm
 
 def calc_view(request):
     result = None
+    normal_result = None
+    dv = None
     if request.method == "POST":
         form = HeightForm(request.POST)
         if form.is_valid():
@@ -12,9 +14,14 @@ def calc_view(request):
             # разные формулы по полу (пример)
             if gender == "male":
                 result = height * 0.7 + 11.2
+                normal_result =  14.3134 + 0.637057 * height
+                dv = result / normal_result * 100
             else:
                 result = height * 0.7
+                normal_result = -0.43188 + 0.664578 * height
+                dv = result / normal_result * 100
+
     else:
         form = HeightForm()
 
-    return render(request, "main/main.html", {"form": form, "result": result})
+    return render(request, "main/main.html", {"form": form, "result": result, "normal_result": normal_result, "dv": dv})
